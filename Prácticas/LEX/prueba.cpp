3b1b3360d308e2ca0992f6a6df5a34d879f9e301
elenacm <eleniita2197@gmail.com>
2018-12-18 12:33:34 +0100

    Terminada practica LEX

--- Prácticas/LEX/prueba.cpp
+++ Prácticas/LEX/prueba.cpp
@@ -1,23 +1,25 @@
-//Por aquí un código que no representa nada...
+/* 
+	Ejemplo ilustrativo del uso básico de funciones. Declaración
+	y llamada de funciones tanto con parámetros como sin ellos.
+*/ 
+#include <stdlib.h>
 
-int funcionuno ()
-{
- return 1;
-}
+int funcionuno (){ return 1; }
  
- int funciondos ( int x )
- {
- return x+1;
- }
+int funciondos ( int x ){ return x+1; }
 
 int funciontres ( int aa , int ba , int c );
 
-pair<int,char> funcioncuatro ( int a , char ba ) ;
+pair<int,char> funcioncuatro ( int a , char ba );
 
+void main ( int arg , char argv  ){
 
-void main ( int arg , char argv  )
-{
-//Algo de código habrá aquí digo yo
-return 0;
+	// Genera numero aleatorio entre 1 y 10
+	int n = rand()%10 + 1;
+	
+	std::vector<int> v;
+	// Añadimos número generado al final del vector
+	v.push_back(n);
+
+	return 0;
 }
-
