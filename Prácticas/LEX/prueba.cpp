3c02bb7d4b1cdf89d706c3ba405b17c6b88d260a
elenacm <eleniita2197@gmail.com>
2018-12-18 11:22:53 +0100

    Añadiendo código práctica LEX

---
+++ Prácticas/LEX/prueba.cpp
@@ -0,0 +1,23 @@
+//Por aquí un código que no representa nada...
+
+int funcionuno ()
+{
+ return 1;
+}
+ 
+ int funciondos ( int x )
+ {
+ return x+1;
+ }
+
+int funciontres ( int aa , int ba , int c );
+
+pair<int,char> funcioncuatro ( int a , char ba ) ;
+
+
+void main ( int arg , char argv  )
+{
+//Algo de código habrá aquí digo yo
+return 0;
+}
+
